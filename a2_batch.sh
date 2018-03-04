070dd3916b876db0c102370c65ce39cf0745c7b4
Benjamin Slack <iam@nimajneb.com>
2018-03-04 18:34:29 -0500

    tuning

--- a2_batch.sh
+++ a2_batch.sh
@@ -2,9 +2,9 @@
 WAIT=$1;
 for t in 1 2 4 8 16;
 do
-	for n in 1000 1260 1587 2000 2520;
+	for n in 3969 5000 6300 7937 10000;
 	do
-		for chunk in 1 4 16 64;
+		for chunk in 64 128 256 512 1024;
 		do
 			echo "qsub -l nodes=1:ppn=${t} -F \"-n ${n} -t ${t} -s ${chunk}\" a2.pbs";
 			qsub -l nodes=1:ppn=${t} -F "-n ${n} -t ${t} -s ${chunk}" a2.pbs;
