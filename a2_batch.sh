5350cc9903a1ab7b1e1c67b360591f6b3615cee5
Benjamin Slack <iam@nimajneb.com>
2018-03-06 16:55:47 -0500

    batch modification

--- a2_batch.sh
+++ a2_batch.sh
@@ -4,19 +4,26 @@ for t in 1 2 4 8 16;
 do
 	for n in 3969 5000 6300 7937 10000;
 	do
-		for chunk in 64 128 256 512 1024;
-		do
-			echo "qsub -l nodes=1:ppn=${t} -F \"-n ${n} -t ${t} -s ${chunk}\" a2.pbs";
-			qsub -l nodes=1:ppn=${t} -F "-n ${n} -t ${t} -s ${chunk}" a2.pbs;
+		if [ $t -eq 1 ]
+		then
+			echo "qsub -l nodes=1:ppn=${t} -F \"-n ${n} -t ${t} -s 1\" a2.pbs";
+			qsub -l nodes=1:ppn=${t} -F "-n ${n} -t ${t} -s 1" a2.pbs;
 			sleep ${WAIT};
-			
-			echo "qsub -l nodes=1:ppn=${t} -F \"-n ${n} -t ${t} -d ${chunk}\" a2.pbs";
-			qsub -l nodes=1:ppn=${t} -F "-n ${n} -t ${t} -d ${chunk}" a2.pbs;
-			sleep ${WAIT};
-			
-			echo "qsub -l nodes=1:ppn=${t} -F \"-n ${n} -t ${t} -g ${chunk}\" a2.pbs";
-			qsub -l nodes=1:ppn=${t} -F "-n ${n} -t ${t} -g ${chunk}" a2.pbs;
-			sleep ${WAIT};
-		done
+		else		
+			for chunk in 64 128 256 512 1024;
+			do
+				echo "qsub -l nodes=1:ppn=${t} -F \"-n ${n} -t ${t} -s ${chunk}\" a2.pbs";
+				qsub -l nodes=1:ppn=${t} -F "-n ${n} -t ${t} -s ${chunk}" a2.pbs;
+				sleep ${WAIT};
+				
+				echo "qsub -l nodes=1:ppn=${t} -F \"-n ${n} -t ${t} -d ${chunk}\" a2.pbs";
+				qsub -l nodes=1:ppn=${t} -F "-n ${n} -t ${t} -d ${chunk}" a2.pbs;
+				sleep ${WAIT};
+				
+				echo "qsub -l nodes=1:ppn=${t} -F \"-n ${n} -t ${t} -g ${chunk}\" a2.pbs";
+				qsub -l nodes=1:ppn=${t} -F "-n ${n} -t ${t} -g ${chunk}" a2.pbs;
+				sleep ${WAIT};
+			done
+		fi
 	done
 done
